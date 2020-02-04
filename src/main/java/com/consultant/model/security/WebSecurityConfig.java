9ddeaac0f8661da32b6b85da07aa5f179077f1a1
Panos68 <panosst68@gmail.com>
2020-02-04 11:44:49 +0100

    Restriction so users can only be accessed by admin

--- src/main/java/com/consultant/model/security/WebSecurityConfig.java
+++ src/main/java/com/consultant/model/security/WebSecurityConfig.java
@@ -34,7 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/user/authenticate").permitAll().
+                .authorizeRequests().antMatchers("/user/authenticate").permitAll().and().
+                authorizeRequests().antMatchers("/user/**").hasAuthority("admin").
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
