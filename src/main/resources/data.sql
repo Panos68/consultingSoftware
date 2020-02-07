a169cbe6c229f56efb0221a332d1787a1d30a171
Panos68 <panosst68@gmail.com>
2020-02-07 13:06:31 +0100

    Added IT tests for ConsultantController

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,7 @@ INSERT INTO `users` (`id`, `password`, `username`) VALUES ('3', '123', 'UserToDe
 INSERT INTO `users` (`id`, `password`, `username`) VALUES ('4', '123', 'NonAdminUser');
 insert into `user_roles` values ('1','admin');
 insert into `user_roles` values ('4','user');
+
+INSERT INTO `consultants` (`id`, `contract_ending`, `contract_started`, `discount`, `first_name`, `last_name`, `list_price`, `other`, `price`, `signed`, `status`, `updated_contract_ending`) VALUES ('1', null, null, '20', 'Main', 'User', '200', 'Some other text', '100', true, 'active', null);
+INSERT INTO `consultants` (`id`, `contract_ending`, `contract_started`, `discount`, `first_name`, `last_name`, `list_price`, `other`, `price`, `signed`, `status`, `updated_contract_ending`) VALUES ('2', null, null, '20', 'Delete', 'User', '200', 'Some other text', '100', true, 'active', null);
+INSERT INTO `consultants` (`id`, `contract_ending`, `contract_started`, `discount`, `first_name`, `last_name`, `list_price`, `other`, `price`, `signed`, `status`, `updated_contract_ending`) VALUES ('3', null, null, '20', 'Edit', 'User', '200', 'Some other text', '100', true, 'active', null);
---
+++ src/test/java/ConsultantControllerIT.java
@@ -0,0 +1,99 @@
+import com.consultant.model.dto.ConsultantDTO;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConsultantControllerIT extends ControllerITtest {
+
+    Gson gson = new Gson();
+
+    @Test
+    public void testConsultantsRetrieving() throws Exception {
+        addAuthorizationRequestToHeader();
+
+        HttpEntity<ConsultantDTO> getConsultantsEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/consultants"), HttpMethod.GET, getConsultantsEntity, String.class);
+        Type type = new TypeToken<List<ConsultantDTO>>() {}.getType();
+
+        List<ConsultantDTO> consultantDTOS = gson.fromJson(response.getBody(), type);
+
+        assertTrue(Objects.requireNonNull(consultantDTOS).stream().map(ConsultantDTO::getId).anyMatch(u -> u.equals(1L)));
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+
+    @Test
+    public void testConsultantCreation() throws Exception {
+        ConsultantDTO consultantDTO = new ConsultantDTO();
+        consultantDTO.setFirstName("Created");
+
+        addAuthorizationRequestToHeader();
+
+        HttpEntity<ConsultantDTO> entity = new HttpEntity<>(consultantDTO, headers);
+        ResponseEntity<String> createConsultantResponse = restTemplate.exchange(
+                createURLWithPort("/consultants"), HttpMethod.POST, entity, String.class);
+
+        HttpEntity<ConsultantDTO> getConsultantsEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/consultants"), HttpMethod.GET, getConsultantsEntity, String.class);
+        Type type = new TypeToken<List<ConsultantDTO>>() {}.getType();
+
+        List<ConsultantDTO> consultantDTOS = gson.fromJson(response.getBody(), type);
+
+        assertTrue(Objects.requireNonNull(consultantDTOS).stream().map(ConsultantDTO::getFirstName).anyMatch(u -> u.equals("Created")));
+        assertEquals(createConsultantResponse.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void testConsultantDeletion() throws Exception {
+        addAuthorizationRequestToHeader();
+
+        HttpEntity<ConsultantDTO> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> deleteUserResponse = restTemplate.exchange(
+                createURLWithPort("/consultants/3"), HttpMethod.DELETE, entity, String.class);
+
+        HttpEntity<ConsultantDTO> getConsultantsEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/consultants"), HttpMethod.GET, getConsultantsEntity, String.class);
+        Type type = new TypeToken<List<ConsultantDTO>>() {}.getType();
+
+        List<ConsultantDTO> consultantDTOS = gson.fromJson(response.getBody(), type);
+
+        assertFalse(Objects.requireNonNull(consultantDTOS).stream().map(ConsultantDTO::getFirstName).anyMatch(u -> u.equals("Delete")));
+        assertEquals(deleteUserResponse.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void testConsultantEditing() throws Exception {
+        addAuthorizationRequestToHeader();
+
+        ConsultantDTO consultantDTO = new ConsultantDTO();
+        consultantDTO.setId(2L);
+        consultantDTO.setFirstName("EditedUser");
+
+        HttpEntity<ConsultantDTO> entity = new HttpEntity<>(consultantDTO, headers);
+        ResponseEntity<String> deleteUserResponse = restTemplate.exchange(
+                createURLWithPort("/consultants"), HttpMethod.PUT, entity, String.class);
+
+        HttpEntity<ConsultantDTO> getConsultantsEntity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/consultants"), HttpMethod.GET, getConsultantsEntity, String.class);
+        Type type = new TypeToken<List<ConsultantDTO>>() {}.getType();
+
+        List<ConsultantDTO> consultantDTOS = gson.fromJson(response.getBody(), type);
+
+        assertTrue(Objects.requireNonNull(consultantDTOS).stream().map(ConsultantDTO::getFirstName).anyMatch(u -> u.equals("EditedUser")));
+        assertEquals(deleteUserResponse.getStatusCode(), HttpStatus.OK);
+    }
+}
