83527fead1c4dffdeb9b4b5e62e037d683a396a9
Anna Wrang <anna.wrang@gmail.com>
2020-10-26 20:42:44 +0100

    make user select account when logging in

--- src/main/java/com/consultant/model/security/WebSecurityConfig.java
+++ src/main/java/com/consultant/model/security/WebSecurityConfig.java
@@ -9,8 +9,16 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.session.SessionManagementFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +30,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomLogoutSuccessHandler logoutSuccessHandler;
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Bean
     CorsFilter corsFilter() {
@@ -47,6 +58,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
+                .authorizationEndpoint()
+                .authorizationRequestResolver(new CustomAuthorizationRequestResolver(this.clientRegistrationRepository))
+                .and()
 
                 // will be invoked after successful login
                 .successHandler(authenticationSuccessHandler)
@@ -61,6 +75,54 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .clearAuthentication(true)
                 .logoutSuccessHandler(logoutSuccessHandler)
                 .deleteCookies("JSESSIONID").permitAll();
+    }
+
+
+    public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
+        private final OAuth2AuthorizationRequestResolver defaultAuthorizationRequestResolver;
+
+        public CustomAuthorizationRequestResolver(
+                ClientRegistrationRepository clientRegistrationRepository) {
+
+            this.defaultAuthorizationRequestResolver =
+                    new DefaultOAuth2AuthorizationRequestResolver(
+                            clientRegistrationRepository, "/oauth2/authorization");
+        }
+
+        @Override
+        public OAuth2AuthorizationRequest resolve(HttpServletRequest request) {
+            OAuth2AuthorizationRequest authorizationRequest =
+                    this.defaultAuthorizationRequestResolver.resolve(request);
+
+            return authorizationRequest != null ?
+                    customAuthorizationRequest(authorizationRequest) :
+                    null;
+        }
+
+        @Override
+        public OAuth2AuthorizationRequest resolve(
+                HttpServletRequest request, String clientRegistrationId) {
+
+            OAuth2AuthorizationRequest authorizationRequest =
+                    this.defaultAuthorizationRequestResolver.resolve(
+                            request, clientRegistrationId);
+
+            return authorizationRequest != null ?
+                    customAuthorizationRequest(authorizationRequest) :
+                    null;
+        }
+
+        private OAuth2AuthorizationRequest customAuthorizationRequest(
+                OAuth2AuthorizationRequest authorizationRequest) {
+
+            Map<String, Object> additionalParameters =
+                    new LinkedHashMap<>(authorizationRequest.getAdditionalParameters());
+            additionalParameters.put("prompt", "select_account");
+
+            return OAuth2AuthorizationRequest.from(authorizationRequest)
+                    .additionalParameters(additionalParameters)
+                    .build();
+        }
     }
 
 }
